7f3f65009a9e04a12f14dc9486b9372d22fe0ce0
Arianna Masciolini <uzkamascio@gmail.com>
2021-05-03 16:10:44 +0200

    fixed indentation

--- FastAlignUtils.hs
+++ FastAlignUtils.hs
@@ -13,11 +13,11 @@ import ConceptAlignment
 -- correspondences 
 parsePh :: String -> [[(Int,[Int])]]
 parsePh s = map (oneToMany . map toIdxPair . words) (lines s)
-    where 
-        toIdxPair :: String -> (Int,[Int])
-        toIdxPair p = (read i :: Int, [read j :: Int])
-            where [i,j] = splitOn "-" p
-        oneToMany = M.toList . M.fromListWith (++)
+  where 
+    toIdxPair :: String -> (Int,[Int])
+    toIdxPair p = (read i :: Int, [read j :: Int])
+      where [i,j] = splitOn "-" p
+    oneToMany = M.toList . M.fromListWith (++)
 
 -- | Parse a bitext in fast_align format to a list of pairs of sentences
 parseBi :: String -> [(String, String)]
@@ -29,33 +29,36 @@ parseBi s = [(head s',last s') | s' <- map (splitOn " ||| ") (lines s)]
 phFileToAlignments :: [(String,String)] -> [[(Int,[Int])]] -> AlignmentMap
 phFileToAlignments _ [] = M.empty -- length is supposed to be the same though
 phFileToAlignments [] _ = M.empty
-phFileToAlignments (s:ss) (ps:pss) = phSentToAlignment s ps `union'` phFileToAlignments ss pss
-    where
-        phSentToAlignment :: (String,String) -> [(Int,[Int])] -> AlignmentMap
-        phSentToAlignment _ [] = M.empty
-        phSentToAlignment (src,trg) ((i,js):ps) = a `insert'` phSentToAlignment (src,trg) ps
-            where 
-                a = alignment2keyval $ (initAlignment $ AT (t,u)) { 
-                        meta = initMeta {
-                            reasons = S.singleton FAST
-                        }
-                }
-                (t,u) = (mockUDTree one,mockUDTree many) 
-                mockUDTree x = RTree ((initUDWord 1) { udFORM = filter (/= '\n') x }) []
-                one = words src !! i
-                many = unlines $ reverse $ intersperse " " [words trg !! j | j <- js]
+phFileToAlignments (s:ss) (ps:pss) = 
+    phSentToAlignment s ps `union'` phFileToAlignments ss pss
+  where
+    phSentToAlignment :: (String,String) -> [(Int,[Int])] -> AlignmentMap
+    phSentToAlignment _ [] = M.empty
+    phSentToAlignment (src,trg) ((i,js):ps) = 
+        a `insert'` phSentToAlignment (src,trg) ps
+      where 
+        a = alignment2keyval $ (initAlignment $ AT (t,u)) { 
+            meta = initMeta {
+              reasons = S.singleton FAST
+            }
+        }
+        (t,u) = (mockUDTree one,mockUDTree many) 
+        mockUDTree x = 
+            RTree ((initUDWord 1) { udFORM = filter (/= '\n') x }) []
+        one = words src !! i
+        many = unlines $ reverse $ intersperse " " [words trg !! j | j <- js]
 
 -- | CoNLL-U aligned strings to fast-align bitext conversion (useful to feed 
 --   fast_aling the same tokens)
 conllu2bi :: (String,String) -> String
 conllu2bi (c1,c2) = unlines $ zipWith (\s1 s2 -> s1 ++ " ||| " ++ s2) ts1 ts2
-    where
-        (ss1,ss2) = mapPair parseUDText (c1,c2) 
-        (ts1,ts2) = mapPair (map (unwords . getTks . udWordLines)) (ss1,ss2)
-            where
-                getTks [] = []
-                getTks (x:xs) = udFORM x:getTks xs'
-                  where xs' = case udID x of
-                                  (UDIdRange n m) -> drop (m - n + 1) xs
-                                  _ -> xs
-        mapPair f (x,y) = (f x, f y)
+  where
+    (ss1,ss2) = mapPair parseUDText (c1,c2) 
+    (ts1,ts2) = mapPair (map (unwords . getTks . udWordLines)) (ss1,ss2)
+      where
+        getTks [] = []
+        getTks (x:xs) = udFORM x:getTks xs'
+          where xs' = case udID x of
+                  (UDIdRange n m) -> drop (m - n + 1) xs
+                  _ -> xs
+    mapPair f (x,y) = (f x, f y)
