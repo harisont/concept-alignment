922afbf7e4c4844e96702790a94c650bc54c3102
Arianna Masciolini <uzkamascio@gmail.com>
2021-07-15 14:55:08 +0200

    compilable skeleton for new eval script

--- Eval.hs
+++ Eval.hs
@@ -1,50 +1,54 @@
 module Eval where
 
-import Data.List
+--import Data.List
+import Data.Maybe
 import System.Exit
 import System.Environment (getArgs)
 import System.Console.GetOpt
-import ArgvParse
 import UDConcepts
+import ArgvParse
+import ConceptAlignment
 
--- | Evaluation script
 main = do
   argv <- getArgs
   (flags,args) <- parseArgv argv help options
   if Help `elem` flags 
     then putStrLn help >> exitSuccess 
     else do
-      alignments <- case args of
-        [p] -> parseUDFile p 
-        (cmd:p1:p2:_) -> do
-          olds <- parseUDFile p1 
-          news <- parseUDFile p2 
-          news' <- if all isAnnotated news
-                    then return news
-                    else annotate (reverse olds) (reverse news) 
-          case cmd of 
-            "extraction" -> putStrLn $ diffStats olds news
-            "propagation" -> putStrLn $ propStats olds news
-          writeFile p2 (unlines $ map prt (reverse news'))
-          return news
+      as <- case args of 
+        [n1,n2] -> getAlignmentsFromUDFiles n1 n2
+        [cmd,o1,o2,n1,n2] -> do
+          olds <- getAlignmentsFromUDFiles o1 o2
+          news <- getAlignmentsFromUDFiles n1 n2
+          news' <- if all isAnnotated news 
+                    then return news 
+                    else annotate olds news
+          case cmd of
+            "extraction" -> putStrLn "TODO:"
+            "propagation" -> putStrLn "TODO:"
+          writeFile n1 (unlines $ map (show . fst) news')
+          writeFile n2 (unlines $ map (show . snd) news')
+          return news'
         _ -> do
           putStrLn "Wrong number of arguments."
           putStrLn help
           exitWith (ExitFailure 1)
-      putStrLn $ basicStats alignments
-      if Reasons `elem` flags 
-        then putStrLn $ reasonStats alignments
-        else putStrLn ""
+      putStrLn "TODO:"
 
-isAnnotated :: UDSentence -> Bool
-isAnnotated s = any (\l -> "# correctness:" `isPrefixOf` l) (udCommentLines s)
+type Path = String
 
-annotate = undefined
+getAlignmentsFromUDFiles :: Path -> Path -> IO [Alignment]
+getAlignmentsFromUDFiles p1 p2 = do 
+  p1' <- parseUDFile p1
+  p2' <- parseUDFile p2
+  return $ zipWith (curry sentencePair2alignment) p1' p2'
 
-basicStats = undefined
-reasonStats = undefined
-diffStats = undefined
-propStats = undefined
+
+isAnnotated :: Alignment -> Bool
+isAnnotated (_,m) = isJust $ correctness m
+
+annotate :: [Alignment] -> [Alignment] -> IO [Alignment]
+annotate = undefined 
 
 {- Argument parsing -} 
 
@@ -56,8 +60,8 @@ options =
 
 help :: String
 help = usageInfo 
-        ("Usage: stack exec -- EvAlign annotated.conllu [flags], or\n"
-     ++ "        stack exec -- EvAlign extraction annotated.conllu new.conllu [flags], or"
-     ++ "        stack exec -- EvAlign propagation annotated.conllu new.conllu [flags] "
-     ++ "(NOTE: new.conllu does not need to be pre-annotated)")
+        ("Usage: stack exec -- EvAlign annotatedSL.conllu annotatedTL.conllu [flags], or\n"
+     ++ "        stack exec -- EvAlign extraction annotatedSL.conllu annotatedTL.conllu newSL.conllu newTL.conllu [flags], or"
+     ++ "        stack exec -- EvAlign propagation annotatedSL.conllu annotatedTL.conllu newSL.conllu newTL.conllu [flags] "
+     ++ "(NOTE: newXX.conllu does not need to be pre-annotated)")
         options
