a4e205055a168add4cd7492ee3b2f66a8479b71a
Arianna Masciolini <uzkamascio@gmail.com>
2021-07-15 17:13:02 +0200

    quickly adapted the old stats functions

--- Eval.hs
+++ Eval.hs
@@ -1,6 +1,7 @@
 module Eval where
 
 import Data.List
+import Data.Set (size, toList)
 import Data.Maybe
 import System.Exit
 import System.Environment (getArgs)
@@ -73,10 +74,107 @@ annotate olds (new:news) = do
         else annotateManually new
 
 {- Statistics -}
-diffStats = undefined
-propStats = undefined
-basicStats = undefined
-reasonStats = undefined
+
+-- | Compare two (annotated) .ca "files" (used to evaluate CE)
+diffStats :: [Alignment] -> [Alignment] -> String
+diffStats olds news = unlines
+  [showLength (filter isCorrect lost) ++ " correct alignments lost",
+   showLength (filter isIncorrect lost) ++ " incorrect alignments lost",
+   showLength (filter isCorrect found) ++ " correct alignments found",
+   showLength (filter isIncorrect found) ++ " incorrect alignments found"]
+  where 
+    lost = olds \\ news
+    found = news \\ olds
+
+-- | Compare two (annotated) .ca "files" (used to evaluate CP)
+propStats :: [Alignment] -> [Alignment] -> String
+propStats origs props = unlines
+  [
+    showLength props ++ "/" ++ showLength origs ++ " (" 
+    ++ show (listPercent props origs) ++ "%) concepts propagated"
+    ++ "\n  of which " ++ showLength correct ++ " (" 
+    ++ show (listPercent correct props) ++ "%) correctly"
+    ++ "\n  of the " ++ showLength incorrect ++ " (" 
+    ++ show (listPercent incorrect props) ++ "%)"
+    ++ " incorrect alignments, " ++ showLength newIncorrect ++ " (" 
+    ++ show (listPercent newIncorrect incorrect) ++ "%) introduce alignment "
+    ++ "errors that are not present in the extracted concepts."
+  ]
+  where
+    correct = filter isCorrect props
+    incorrect = filter isIncorrect props
+    newIncorrect = filter isNewError incorrect
+      where 
+        -- weak check!
+        isNewError e = isCorrect orig
+          where orig = fromJust $ 
+                  find (\a -> tl a == sl e || sl a == tl e) origs
+
+-- | Stats for a single (annotated) .ca file
+basicStats :: [Alignment] -> String
+basicStats alignments = 
+  "\ntotal number of alignments extracted: " ++ show totAligns
+  ++ "\n  of which " ++ showLength distinct ++ " distinct"
+  ++ "\n    of which " ++ showLength correct 
+  ++ " (" ++ show correctRatio ++ "%) correct"
+  ++ "\n    of which " ++ showLength useful 
+  ++ " (" ++ show usefulRatio ++ "%) potentially useful!" ++ "\n" 
+  where 
+    totAligns = sum $ map (size . sentIds . meta) alignments
+    -- filterings
+    distinct = nubBy (\a b -> trees a == trees b) alignments
+    correct = filter isCorrect distinct
+    useful = filter isUseful correct
+    incorrect = alignments \\ correct
+    -- percentages
+    correctRatio = listPercent correct alignments
+    usefulRatio = listPercent useful alignments
+
+reasonStats :: [Alignment] -> String
+reasonStats alignments = concatMap reasonStats' (powerlist rList)
+  where
+    -- list of reasons (obtained automagically)
+    rList = enumFrom minBound :: [Reason] 
+    -- reason-based stats 
+    reasonStats' :: [Reason] -> String
+    reasonStats' r = if rRatio r /= 0
+      then "\n" ++ show r ++ " based alignments: " ++ show (rRatio r) ++ "%" 
+           ++ "\n  of which " ++ show (rCorrectRatio r) ++ "% correct"
+      else ""
+    becauseOf r = filter (isBecauseOf r) alignments
+    rRatio r = listPercent (becauseOf r) alignments
+    rCorrectRatio r = listPercent (filter isCorrect (filter isCorrect becauseOfR)) becauseOfR
+      where becauseOfR = becauseOf r
+    powerlist :: [a] -> [[a]]
+    powerlist [] = [[]]
+    powerlist (x:xs) = [x:ps | ps <- powerlist xs] ++ powerlist xs
+
+-- | Check if alignment is marked as correct (+ or =)
+isCorrect :: Alignment -> Bool
+isCorrect a = fromJust (correctness (meta a)) `elem` [Specific, Correct]
+
+-- | Check if alignment is marked as incorrect (-)
+isIncorrect :: Alignment -> Bool
+isIncorrect = not . isCorrect
+
+-- | Check if alignment is marked as useful 
+-- (i.e. correct and not too context specific)
+isUseful :: Alignment -> Bool
+isUseful a = fromJust (correctness (meta a)) == Correct
+
+-- | Check if an alignment has been found because of the given reasons
+isBecauseOf :: [Reason] -> Alignment -> Bool
+isBecauseOf rs a = toList (reasons $ meta a) == rs
+
+-- | Helper to show lengths
+showLength :: [a] -> String
+showLength = show . length
+
+-- | Helper to compute %s of alignments from lists
+listPercent :: [a] -> [a] -> Float
+listPercent n d = 
+  (fromIntegral (length n) / fromIntegral (length d)) * 100
+
 
 {- Argument parsing -} 
 
